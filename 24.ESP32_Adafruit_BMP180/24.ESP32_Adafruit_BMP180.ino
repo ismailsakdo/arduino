e0160ca11ab1fbf298c1b7e31cd3b90bd7cbc9e4
Ismail SA Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2021-07-08 17:11:45 +0800

    Add files via upload

---
+++ 24.ESP32_Adafruit_BMP180/24.ESP32_Adafruit_BMP180.ino
@@ -0,0 +1,85 @@
+/////////////////////////////////
+// Generated with a lot of love//
+// with TUNIOT FOR ESP32     //
+// Website: Easycoding.tn      //
+/////////////////////////////////
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <WiFi.h>
+#include <Adafruit_BMP085.h>
+#include <Wire.h>
+Adafruit_BMP085 bmp;
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME  "ismailsa"
+#define AIO_KEY  "aio_WTVV56tDeFKZvDs7fkR3FlZ6E0As"
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+boolean MQTT_connect();
+
+boolean MQTT_connect() {  int8_t ret; if (mqtt.connected()) {    return true; }  uint8_t retries = 3;  while ((ret = mqtt.connect()) != 0) { mqtt.disconnect(); delay(2000);  retries--;if (retries == 0) { return false; }} return true;}
+
+Adafruit_MQTT_Publish Temperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Temperature");
+Adafruit_MQTT_Publish Pressure = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Pressure");
+Adafruit_MQTT_Publish Altitude = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/Altitude");
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  bmp.begin();
+  WiFi.disconnect();
+  delay(3000);
+  Serial.println("START");
+  WiFi.begin("aizatwifi2.5@unifi","11Septem!");
+  while ((!(WiFi.status() == WL_CONNECTED))){
+    delay(300);
+    Serial.print("..");
+
+  }
+  Serial.println("Connected");
+  Serial.println("Your IP is");
+  Serial.println((WiFi.localIP()));
+
+}
+
+
+void loop()
+{
+
+    float temp = bmp.readTemperature();
+    float pres = bmp.readPressure();
+    float alt = bmp.readAltitude();
+    
+    if (MQTT_connect()) {
+      if (Temperature.publish(temp)) {
+        Serial.println("Sent!!!");
+        Serial.println(temp);
+
+      } else {
+        Serial.println("Error");
+
+      }
+      if (Pressure.publish(pres)) {
+        Serial.println("Sent!!!");
+        Serial.println(pres);
+
+      } else {
+        Serial.println("Error");
+
+      }
+      if (Altitude.publish(alt)) {
+        Serial.println("Sent!!!");
+        Serial.println(alt);
+
+      } else {
+        Serial.println("Error");
+
+      }
+
+    }
+    delay(300000);
+
+}
